82ce51cd8c610c0a9f119a7a94bd4100b95e0ba9
Windows 10 <yasmanic17@gmail.com>
2022-04-19 10:39:56 -0400

    llenado de materias

--- database/seeders/MateriaSeeder.php
+++ database/seeders/MateriaSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class MateriaSeeder extends Seeder
 {
@@ -13,6 +15,24 @@ class MateriaSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $data=[
+            ['descripcion'=>'introduccion a la programacion','departamento'=>'sistemas','created_at' => now(),'updated_at' => now()],
+            ['descripcion'=>'elementos de programacion estructura de datos','departamento'=>'sistemas','created_at' => now(),'updated_at' => now()],
+            ['descripcion'=>'base de datos I','departamento'=>'sistemas','created_at' => now(),'updated_at' => now()],
+            ['descripcion'=>'base de datos II a la programacion','departamento'=>'sistemas','created_at' => now(),'updated_at' => now()],
+            ['descripcion'=>'sistemas de informacion I','departamento'=>'sistemas','created_at' => now(),'updated_at' => now()],
+            ['descripcion'=>'inteligencia artificial','departamento'=>'sistemas','created_at' => now(),'updated_at' => now()],
+            ['descripcion'=>'ingenieria de software','departamento'=>'sistemas','created_at' => now(),'updated_at' => now()],
+            ['descripcion'=>'taller de ingenieria de software','departamento'=>'sistemas','created_at' => now(),'updated_at' => now()],
+            ['descripcion'=>'sistemas de informacion II','departamento'=>'sistemas','created_at' => now(),'updated_at' => now()],
+            ['descripcion'=>'taller de metodos de programacion','departamento'=>'sistemas','created_at' => now(),'updated_at' => now()],
+            ['descripcion'=>'taller de base de datos','departamento'=>'sistemas','created_at' => now(),'updated_at' => now()],
+            ['descripcion'=>'taller de ingenieria de software','departamento'=>'sistemas','created_at' => now(),'updated_at' => now()],
+            ['descripcion'=>'entornos virtuales de aprendizaje','departamento'=>'sistemas','created_at' => now(),'updated_at' => now()],
+            ['descripcion'=>'reconocimiento de voz','departamento'=>'sistemas','created_at' => now(),'updated_at' => now()],
+            ['descripcion'=>'datawarehouse','departamento'=>'sistemas','created_at' => now(),'updated_at' => now()],
+
+        ];
+        DB::table('materias')->insert($data);
     }
 }
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,11 +1,11 @@
 <?php return array(
     'root' => array(
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => NULL,
+        'reference' => '3dba87f3b182d55f7e2028e551cb198e02873edc',
         'name' => 'laravel/laravel',
         'dev' => true,
     ),
@@ -404,12 +404,12 @@
             'dev_requirement' => false,
         ),
         'laravel/laravel' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => NULL,
+            'reference' => '3dba87f3b182d55f7e2028e551cb198e02873edc',
             'dev_requirement' => false,
         ),
         'laravel/legacy-factories' => array(
