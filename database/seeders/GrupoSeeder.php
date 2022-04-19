709082984e8ff676791ae2b3058c9e4c1b51595e
Windows 10 <yasmanic17@gmail.com>
2022-04-19 10:49:14 -0400

    datos grupos

--- database/seeders/GrupoSeeder.php
+++ database/seeders/GrupoSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class GrupoSeeder extends Seeder
 {
@@ -13,6 +15,17 @@ class GrupoSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $data=[
+            ['grupo'=>'1A','materia'=>'1','usuario'=>'2','inscritos'=>'30','created_at' => now(),'updated_at' => now()],
+            ['grupo'=>'2A','materia'=>'1','usuario'=>'2','inscritos'=>'50','created_at' => now(),'updated_at' => now()],
+            ['grupo'=>'4','materia'=>'2','usuario'=>'2','inscritos'=>'25','created_at' => now(),'updated_at' => now()],
+            ['grupo'=>'4','materia'=>'2','usuario'=>'2','inscritos'=>'20','created_at' => now(),'updated_at' => now()],
+            ['grupo'=>'1','materia'=>'3','usuario'=>'2','inscritos'=>'40','created_at' => now(),'updated_at' => now()],
+            ['grupo'=>'1','materia'=>'3','usuario'=>'2','inscritos'=>'35','created_at' => now(),'updated_at' => now()],
+
+
+        ];
+        DB::table('grupos')->insert($data);
+
     }
 }
