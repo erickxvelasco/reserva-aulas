d7271312ee60b9454d28d32e46aa71cf13158ba7
Windows 10 <yasmanic17@gmail.com>
2022-04-21 18:57:44 -0400

    registros para tabla gestion

--- database/seeders/GestionSeeder.php
+++ database/seeders/GestionSeeder.php
@@ -1,9 +1,9 @@
 <?php
 
 namespace Database\Seeders;
-
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Seeder;
-
+use Database\Seeders\format;
 class GestionSeeder extends Seeder
 {
     /**
@@ -13,6 +13,15 @@ class GestionSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $data=[
+            ['estado'=>'1','detalle'=>'primer semestre','inicio'=>"2022-02-03",'final'=>"2022-16-07",'created_at' => now(),'updated_at' => now()],
+            ['estado'=>'0','detalle'=>'invierno','inicio'=>"2022-20-07",'final'=>"2022-20-08",'created_at' => now(),'updated_at' => now()],
+            ['estado'=>'0','detalle'=>'segundo semestre','inicio'=>"2022-28-08",'final'=>"2022-20-12",'created_at' => now(),'updated_at' => now()],
+            ['estado'=>'0','detalle'=>'verano','inicio'=>"2023-24-01",'final'=>"2023-19-02",'created_at' => now(),'updated_at' => now()],
+            ['estado'=>'0','detalle'=>'primer semestre','inicio'=>"2023-02-03",'final'=>"2023-06-07",'created_at' => now(),'updated_at' => now()],
+            ['estado'=>'0','detalle'=>'invierno','inicio'=>"2023-20-07",'final'=>"2023-20-08",'created_at' => now(),'updated_at' => now()],
+            ['estado'=>'0','detalle'=>'segundo semestre','inicio'=>"2023-28-08",'final'=>"2023-20-12",'created_at' => now(),'updated_at' => now()],
+        ];
+        DB::table('gestiones')->insert($data);
     }
 }
