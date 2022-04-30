77e464545164222498e0c7f1d799971368271b7e
erickvladimir <92469554+erickxvelasco@users.noreply.github.com>
2022-04-30 10:28:15 -0400

    erick

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -65,14 +65,14 @@ class UserRequest extends FormRequest
         switch ($this->method()) {
             case "POST": {
                $reglas['password'] = ['required','min:6','max:10'];
-               //$reglas['email'] = ['required', 'email', 'unique:users'];
+               $reglas['email'] = ['required', 'email', 'unique:users'];
             }
             case "PATCH":{
                 $reglas['password']= ['nullable','min:6','max:10'];
                 $reglas['email'] = ['required', 'email', 'unique:users,email,'.$this->route('user.id')];
             }
         }
-        //dd($reglas);
+        dd($reglas);
         return $reglas;
     }
 }
